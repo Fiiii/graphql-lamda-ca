062527742d9c8f04ef01acb6af9acc23ab2ae410
Artur Figiel <arturffigiel@gmail.com>
2017-07-11 14:59:31 +0200

    Intend lines

--- src/graphql/resolvers/Post.js
+++ src/graphql/resolvers/Post.js
@@ -9,19 +9,19 @@ const PostsController = {
 		const URL = `https://www.reddit.com/r/${ args.subreddit || 'javascript' }.json`;
 
 		return axios.get( URL )
-			.then( (response) => {
-				const __posts = [];
-				const posts = response.data.data.children;
+		.then( (response) => {
+			const __posts = [];
+			const posts = response.data.data.children;
 
-				posts.map( post => {
-					post.data.content = post.data.selftext_html;
-					__posts.push( post.data );
-				} );
-				return __posts;
-			})
-			.catch( (error) => {
-				return { error: error }
-			});
+			posts.map( post => {
+				post.data.content = post.data.selftext_html;
+				__posts.push( post.data );
+			} );
+			return __posts;
+		})
+		.catch( (error) => {
+			return { error: error }
+		});
 
 	}
 
